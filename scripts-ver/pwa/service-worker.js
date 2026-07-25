bf65b3c1b1c655aff13c7e79cfe67c21875e4b11
naru sue <naru.s@loose.bz>
2026-07-25 11:17:26 +0900

    c-send pwa version new

---
+++ scripts-ver/pwa/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "c-send-pwa-v1.13";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./service-worker.js",
+  "./samples.json",
+  "./apple-touch-icon.png",
+  "./c-send.ico"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const cloned = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, cloned);
+        });
+        return response;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
