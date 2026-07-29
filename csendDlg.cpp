ee9b99a9e0e4753e233dd3f26ea945b75efcd191
naru sue <naru.s@loose.bz>
2026-07-29 12:00:07 +0900

    Revert "fix: keep main window visible when minimized"
    This reverts commit bdd5b8ea950e82deed47a6e9b568cd3c089cfdea.

--- csendDlg.cpp
+++ csendDlg.cpp
@@ -891,10 +891,9 @@ void CCsendDlg::OnSize(UINT nType, int cx, int cy)
 
 	// TODO: この位置にメッセージ ハンドラ用のコードを追加してください
 // Make-->
-	if (nType == SIZE_MINIMIZED) {
-		// 最小化したウインドウを非表示にすると、アプリが終了したように見える。
-		// タスクバーから通常どおり復元できるよう、最小化状態を維持する。
-		return;
+	if( IsIconic() ){	// アイコン化されたら
+		ShowWindow( SW_HIDE );	// ウインドウを非表示にします
+        return;
     }
 	
 	if( !IsZoomed() ){	// 最大化でなければ
